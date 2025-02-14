81e6f39c7b80f9ffe2f5b803d7aa4e8c2380b353
jadewisemann <jadewisemann@gmail.com>
2025-02-14 17:00:15 +0900

    feat: add pareent & command go to parrent

--- main.ts
+++ main.ts
@@ -29,6 +29,7 @@ const dedent = (strings: TemplateStringsArray, ...values: any[]): string => {
     return lines.map(line => line.slice(minIndent)).join('\n')
 }
 
+
 export default class InternalLinkCreator extends Plugin {
     private fileChangeRef: EventRef
 
@@ -39,7 +40,7 @@ export default class InternalLinkCreator extends Plugin {
 
         this.addCommand({
             id: 'make-it-as-internal',
-            name: 'Make selection as internal link',
+            name: '선택한 텍스트를 내부 링크로 만들기',
             editorCallback: async (editor: Editor, view: MarkdownView) => {
                 const selectedText = editor.getSelection()
                 if (!selectedText) {
@@ -49,17 +50,28 @@ export default class InternalLinkCreator extends Plugin {
 
                 try {
                     const currentFile = view.file
-                    const currentFileName = currentFile?.basename
+                    if (!currentFile) {
+                        new Notice('현재 파일을 찾을 수 없습니다')
+                        return
+                    }
+                    
+                    const currentFileName = currentFile.basename
+                    const currentFileCache = this.app.metadataCache.getFileCache(currentFile)
+                    const currentFileAlias = currentFileCache?.frontmatter?.aliases?.[0]
+                    
+                    const prefix = currentFileAlias || currentFileName
+                    const displayAlias = `${prefix}__${selectedText}`
                     
                     const uid = this.generateUID()
-                    const alias = `${currentFileName}__${selectedText}`
+                    const parentLink = await this.createParentLink(currentFile)
                     
                     const newNotePath = `${uid}.md`
                     const newNoteContent = dedent`
                     ---
-                    aliases: ["${alias}"]
-                    parent: [[${currentFileName}]]
+                    aliases: ["${displayAlias}"]
                     ---
+
+                    parent: ${parentLink}
 
                     # ${selectedText}
                     `
@@ -74,6 +86,67 @@ export default class InternalLinkCreator extends Plugin {
                 }
             }
         })
+
+        // 상위 문서로 이동 명령어 추가
+        this.addCommand({
+            id: 'go-to-parent-document',
+            name: '상위 문서로 이동',
+            callback: async () => {
+                const view = this.app.workspace.getActiveViewOfType(MarkdownView)
+                if (!view) {
+                    new Notice("활성화된 마크다운 뷰가 없습니다.")
+                    return
+                }
+                const currentFile = view.file
+                if (!currentFile) {
+                    new Notice("현재 파일을 찾을 수 없습니다.")
+                    return
+                }
+                
+                const fileCache = this.app.metadataCache.getFileCache(currentFile)
+                let parentLink: string | undefined = fileCache?.frontmatter?.parent
+
+                if (!parentLink) {
+                    const content = await this.app.vault.read(currentFile)
+                    const parentLine = content.split("\n").find(line => line.startsWith("parent:"))
+                    if (parentLine) {
+                        parentLink = parentLine.substring("parent:".length).trim()
+                    }
+                }
+
+                if (!parentLink) {
+                    new Notice("상위 문서 정보가 없습니다.")
+                    return
+                }
+
+                const regex = /\[\[([^|\]]+)(?:\|[^\]]+)?\]\]/
+                const match = regex.exec(parentLink)
+                if (!match) {
+                    new Notice("올바른 상위 문서 링크가 아닙니다.")
+                    return
+                }
+                const parentFileName = match[1]
+                
+                const parentFile = this.app.vault.getMarkdownFiles().find(f => f.basename === parentFileName)
+                if (!parentFile) {
+                    new Notice("상위 문서를 찾을 수 없습니다.")
+                    return
+                }
+                
+                this.app.workspace.openLinkText(parentFile.basename, '', false)
+            }
+        })
+    }
+
+    private async createParentLink(file: TFile): Promise<string> {
+        const cache = this.app.metadataCache.getFileCache(file)
+        const parentAlias = cache?.frontmatter?.aliases?.[0]
+        
+        if (parentAlias) {
+            return `[[${file.basename}|${parentAlias}]]`
+        }
+        
+        return `[[${file.basename}]]`
     }
 
     private generateUID(): string {
@@ -84,7 +157,6 @@ export default class InternalLinkCreator extends Plugin {
 
     private async handleFileChange(file: TFile) {
         const content = await this.app.vault.read(file)
-        
         const parts = content.split('---')
         if (parts.length < 3) return
         
@@ -93,30 +165,42 @@ export default class InternalLinkCreator extends Plugin {
         
         if (headingMatch) {
             const newTitle = headingMatch[1]
-            const oldTitle = this.getOldTitle(mainContent)
+            const oldTitle = this.getOldTitle(file)
             
             if (oldTitle && oldTitle !== newTitle) {
                 const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter
                 
-                if (frontmatter?.aliases) {
+                if (frontmatter?.aliases && frontmatter?.parent) {
                     const oldAlias = frontmatter.aliases[0]
                     const [prefix] = oldAlias.split('__')
                     const newAlias = `${prefix}__${newTitle}`
-
-                    const updatedContent = content.replace(
+                    
+                    let updatedContent = content.replace(
                         /aliases: \[".+?"\]/,
                         `aliases: ["${newAlias}"]`
                     )
-                    await this.app.vault.modify(file, updatedContent)
 
+                    const parentMatch = frontmatter.parent.match(/\[\[(.+?)(?:\|.+?)?\]\]/)
+                    if (parentMatch) {
+                        const parentFile = this.app.vault.getMarkdownFiles().find(f => f.basename === parentMatch[1])
+                        if (parentFile) {
+                            const parentLink = await this.createParentLink(parentFile)
+                            updatedContent = updatedContent.replace(
+                                /parent: \[\[.+?\]\]/g,
+                                `parent: ${parentLink}`
+                            )
+                        }
+                    }
+
+                    await this.app.vault.modify(file, updatedContent)
                     await this.updateOriginalFileLink(file.basename, oldAlias, newTitle)
                 }
             }
         }
     }
 
-    private getOldTitle(content: string): string | null {
-        const cache = this.app.metadataCache.getFileCache(this.app.workspace.getActiveFile())
+    private getOldTitle(file: TFile): string | null {
+        const cache = this.app.metadataCache.getFileCache(file)
         if (cache?.headings && cache.headings.length > 0) {
             return cache.headings[0].heading
         }
